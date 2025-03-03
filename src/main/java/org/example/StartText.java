9f03ad20cdb20884a99416f947494752d2618e13
GFR-Q <dikie1920@gmail.com>
2025-03-03 07:50:32 +0500

    убрал не нужный текст и компаненту

--- src/main/java/org/example/StartText.java
+++ src/main/java/org/example/StartText.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class StartText {
     public StartText() {
         System.out.println("---------Tasks List---------");
